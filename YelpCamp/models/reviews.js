8a2ec8fd9d7b6e2fd31adeebb5e85e923c20f183
AlesiaHaxhi <alesiahaxhi@outlook.com>
2021-08-10 19:53:24 +0200

    Capitalization correction in reviewSchema

--- YelpCamp/models/reviews.js
+++ YelpCamp/models/reviews.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ReviewSchema = new Schema ({
+const reviewSchema = new Schema ({
     body: String,
     rating: Number
 })
