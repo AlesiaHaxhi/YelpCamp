ce14ca31350586e6499fe8bf0ff6818373b99180
AlesiaHaxhi <alesiahaxhi@outlook.com>
2021-08-22 20:19:01 +0200

    Flash middleware

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -41,11 +41,18 @@ const sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        expires: Date.now() + 604800000,
-        maxage: 604800000
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig))
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
