4eabdc90cea4ae91d279d87928b11f9d4a0b65e0
AlesiaHaxhi <alesiahaxhi@outlook.com>
2021-08-31 21:13:55 +0200

    Importing validateCampground/Review, isAuthor

--- YelpCamp/middleware.js
+++ YelpCamp/middleware.js
@@ -1,3 +1,9 @@
+const Campground = require('./models/campgrounds');
+const Review = require('./models/reviews');
+
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
@@ -6,3 +12,37 @@ module.exports.isLoggedIn = (req, res, next) => {
     }
     next();
 }
+
+module.exports.validateCampground = (req, res, next) => { 
+    const {error} = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else{
+        next();
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next()
+}
+
+// Review Middleware
+
+module.exports.validateReview = (req, res, next) => { 
+    const {error} = reviewSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else{
+        next();
+    }
+}
+
