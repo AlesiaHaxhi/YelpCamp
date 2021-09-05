1d44971aa3f26c5acd98326ce32fdca904daa1ec
AlesiaHaxhi <alesiahaxhi@outlook.com>
2021-09-05 18:25:16 +0200

    Adding auth and associating a user with a review

--- YelpCamp/routes/reviews.js
+++ YelpCamp/routes/reviews.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router({mergeParams: true});
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn } = require('../middleware');
 
 const Campground = require('../models/campgrounds');
 const Review = require('../models/reviews');
 
 const catchAsync = require('../utils/catchAsync');
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review)
+    review.author = req.user._id;
+    
     campground.reviews.push(review)
-
     await review.save()
     await campground.save()
     req.flash('success', 'Created review')
@@ -19,7 +20,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId }})
     await Review.findByIdAndDelete(reviewId)
