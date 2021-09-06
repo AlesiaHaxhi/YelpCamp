a84a269ae2154742679a0ca12dbbc00d38768a3b
AlesiaHaxhi <alesiahaxhi@outlook.com>
2021-09-06 16:39:43 +0200

    Added isReviewAuthor

--- YelpCamp/middleware.js
+++ YelpCamp/middleware.js
@@ -1,4 +1,5 @@
 const Campground = require('./models/campgrounds');
+const Review = require('./models/reviews');
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 
@@ -36,6 +37,17 @@ module.exports.isAuthor = async (req, res, next) => {
     const campground = await Campground.findById(id)
 
     if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next()
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { reviewId, id } = req.params;
+    const review = await Review.findById(reviewId)
+
+    if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!')
         return res.redirect(`/campgrounds/${id}`)
     }
