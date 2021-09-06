1de85b5aa44a88e2ac41dbda5f10a9bcc7d4299e
AlesiaHaxhi <alesiahaxhi@outlook.com>
2021-09-06 22:36:00 +0200

    Grouping routes

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -5,16 +5,18 @@ const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 
-router.get('/', catchAsync(campgrounds.index))
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(validateCampground, isLoggedIn, catchAsync(campgrounds.createNew))
 
 router.get('/new', isLoggedIn, campgrounds.getNew)
-router.post('/', validateCampground, isLoggedIn, catchAsync(campgrounds.createNew))
 
-router.get('/:id', catchAsync(campgrounds.showInd))
+router.route('/:id')
+    .get(catchAsync(campgrounds.showInd))
+    .put(validateCampground, isLoggedIn, isAuthor, catchAsync(campgrounds.edit))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.delete))
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.getEdited))
-router.put('/:id', validateCampground, isLoggedIn, isAuthor, catchAsync(campgrounds.edit))
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.delete))
 
 module.exports = router;
--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -5,11 +5,14 @@ const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const users = require('../controllers/users');
 
-router.get('/register', users.getSignup)
-router.post('/register', catchAsync(users.signup))
+router.route('/register')
+    .get(users.getSignup)
+    .post(catchAsync(users.signup))
 
-router.get('/login', users.getLogin)
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
+router.route('/login')
+    .get(users.getLogin)
+    .post(passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
+    
 router.get('/logout', users.logout)
 
 module.exports = router;
