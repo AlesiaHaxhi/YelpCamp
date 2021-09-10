eea12655271c3ab27d18e7a0ceb51671d752a544
AlesiaHaxhi <alesiahaxhi@outlook.com>
2021-09-10 17:07:29 +0200

    Adding 3 default pics to all seeds

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -26,10 +26,22 @@ const seedDB = async () => {
         const camp = new Campground({
             author: '6128f9b70d411b229810f467',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, neque dolor, itaque eveniet ipsa eum modi dolorum odio earum magnam nam excepturi harum accusantium asperiores aliquid dolore nesciunt debitis quae.',            
-            price
+            title: `${sample(descriptors)} ${sample(places)}`,            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, neque dolor, itaque eveniet ipsa eum modi dolorum odio earum magnam nam excepturi harum accusantium asperiores aliquid dolore nesciunt debitis quae.',            
+            price,
+            images:  [
+                {
+                  url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1631206021/YelpCamp/y3xhmg6rrojf2hcpl4gg.png',
+                  filename: 'YelpCamp/y3xhmg6rrojf2hcpl4gg'
+                },
+                {
+                  url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1631206028/YelpCamp/nmfygkbpcixisqkgmcov.png',
+                  filename: 'YelpCamp/nmfygkbpcixisqkgmcov'
+                },
+                {
+                  url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1631206026/YelpCamp/lr9bk05btazz1ms658qd.png',
+                  filename: 'YelpCamp/lr9bk05btazz1ms658qd'
+                }
+              ]
         })
         await camp.save();
     }
