a143904f33752bc8db13407c0ecd4328e1e7c45f
AlesiaHaxhi <alesiahaxhi@outlook.com>
2021-09-22 19:34:00 +0200

    Customising popups

--- YelpCamp/models/campgrounds.js
+++ YelpCamp/models/campgrounds.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Review = require('./reviews')
 const Schema = mongoose.Schema;
+const opts = { toJSON: { virtuals: true } };
 
 // https://res.cloudinary.com/dlkvrtnax/image/upload/w_300/v1631640724/YelpCamp/utlkreurzasxfxizbdki.jpg
 
@@ -39,6 +40,13 @@ const campgroundSchema = new Schema ({
             type: Schema.Types.ObjectId,  ref:'Review'  
         }  
     ]
+}, opts)
+
+campgroundSchema.virtual('properties.popUpCode').get(function() {
+    return `
+    <a href="/campgrounds/${this._id}">${this.title}</a>
+    <p>${this.description.substring(0, 20)}...</p>
+    `;
 })
 
 campgroundSchema.post('findOneAndDelete', async function (doc) {
