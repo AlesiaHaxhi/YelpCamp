172f13b143ab7199e1eca83313952b4783d33984
AlesiaHaxhi <alesiahaxhi@outlook.com>
2021-09-23 18:42:12 +0200

    Added new images

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -29,18 +29,22 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, neque dolor, itaque eveniet ipsa eum modi dolorum odio earum magnam nam excepturi harum accusantium asperiores aliquid dolore nesciunt debitis quae.',            
             price,
             images:  [
-                {
-                  url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1631206021/YelpCamp/y3xhmg6rrojf2hcpl4gg.png',
-                  filename: 'YelpCamp/y3xhmg6rrojf2hcpl4gg'
-                },
-                {
-                  url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1631206028/YelpCamp/nmfygkbpcixisqkgmcov.png',
-                  filename: 'YelpCamp/nmfygkbpcixisqkgmcov'
-                },
-                {
-                  url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1631206026/YelpCamp/lr9bk05btazz1ms658qd.png',
-                  filename: 'YelpCamp/lr9bk05btazz1ms658qd'
-                }
+              {
+                url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1632415093/YelpCamp/fcbnsqb7dy3i6rhzmtqe.jpg',
+                filename: 'YelpCamp/fcbnsqb7dy3i6rhzmtqe'
+              },
+              {
+                url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1632415094/YelpCamp/hej23nk9fimoxedxjnz6.jpg',
+                filename: 'YelpCamp/hej23nk9fimoxedxjnz6'
+              },
+              {
+                url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1632415094/YelpCamp/itpreiocidgx1ipjeuzu.jpg',
+                filename: 'YelpCamp/itpreiocidgx1ipjeuzu'
+              },
+              {
+                url: 'https://res.cloudinary.com/dlkvrtnax/image/upload/v1632415094/YelpCamp/mh8v3gcs1lhbuu4qck2l.jpg',
+                filename: 'YelpCamp/mh8v3gcs1lhbuu4qck2l'
+              }
               ],
               geometry: { 
                 type : "Point", 
